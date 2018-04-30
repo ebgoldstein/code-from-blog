7ac5d7bab73f6124e950cd061030bea52ea741bc
Evan B. Goldstein <ebgoldstein@gmail.com>
2018-04-30 16:04:30 -0400

    code outline

---
+++ AGUtweets/AGUtweets2017.R
@@ -0,0 +1,12 @@
+
+#download 2017 AGU twitter data from:
+#Pikas, Christina (2018): American Geophysical Union Annual Meeting Tweets 2017. 
+#figshare. Dataset. https://doi.org/10.6084/m9.figshare.5756514.v1 
+
+data <- read.csv("https://ndownloader.figshare.com/files/10141338")
+
+#correct the time stamps
+
+#plot hourly tweets
+
+#subset for Meeting dates
