c118de557777dd40f75a2623d53a19399372ee7f
Evan B. Goldstein <ebgoldstein@gmail.com>
2018-04-30 20:32:19 -0400

    cleaning

--- AGUtweets/AGUtweets2017.R
+++ AGUtweets/AGUtweets2017.R
@@ -2,10 +2,20 @@
 #download 2017 AGU twitter data from:
 #Pikas, Christina (2018): American Geophysical Union Annual Meeting Tweets 2017. 
 #figshare. Dataset. https://doi.org/10.6084/m9.figshare.5756514.v1 
-
+library(tidyverse)
+library(lubridate)
 data <- read.csv("https://ndownloader.figshare.com/files/10141338")
 
-#correct the time stamps
+#subset columns i want
+datasubset <- select(data, from_user, time)
+
+#reformat the column types and correct the timestamps
+datasubset <- datasubset %>% mutate_at(vars(from_user), as.character)
+datasubset <- datasubset %>% mutate_at(vars(time), dmy_hms)
+
+#mean # of tweets/ person
+
+#distribution of tweets per person 
 
 #plot hourly tweets
 
